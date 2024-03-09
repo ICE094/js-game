aab9a97ce3a823358efe8c352f9aa74a2258560e
ICE094 <atsu94w@gmail.com>
2024-03-09 16:54:47 +0900

    add shop

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const additionbutton = document.getElementById("addition-btn");
 const buybutton = document.getElementById("buy-btn");
+const displaybutton = document.getElementById("display-btn");
+const shop = document.getElementById("shop");
+
 const number = document.getElementById("counter");
 
 let maincount = 0;
@@ -19,6 +22,22 @@ buybutton.addEventListener(
   function () {
     levelcount++;
     buybutton.textContent = `level${levelcount}`;
+  },
+  false
+);
+
+displaybutton.addEventListener(
+  "click",
+  function () {
+    if (shop.style.display == "block") {
+      shop.style.display = "none";
+      displaybutton.classList.remove("X");
+      displaybutton.classList.add("hamburger");
+    } else {
+      shop.style.display = "block";
+      displaybutton.classList.add("X");
+      displaybutton.classList.remove("hamburger");
+    }
   },
   false
 );
