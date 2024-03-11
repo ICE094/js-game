4bba7d8728da3457c718d5cb2e414ae8b6fa5b77
ICE094 <atsu94w@gmail.com>
2024-03-12 00:42:46 +0900

    make the drawer menu

--- main.js
+++ main.js
@@ -1,44 +1,63 @@
-const AdditionButton = $("#addition-btn");
-const BuyButton = $("#buy-btn");
-const DisplayButton = $("#display-btn");
-const shop = $("#shop");
+const additionButton = $("#addition-btn");
 const header = $(".header");
-const MainButton = $(".main-btn");
+const shop = $("#shop");
+const displayButton = $("#display-btn");
+const buyButton = $("#buy-btn");
+const levelUpCost = $("#levelup-cost");
+const buyButton2 = $("#buy-btn2");
+const levelUpCost2 = $("#levelup-cost2");
 
 const number = $("#counter");
 
-let maincount = 0;
-let levelcount = 1;
+let mainCount = 0;
 let intervalID = 0;
+let levelCount = 1;
+let levelCount2 = 1;
+let levelCostCount = 10;
+let levelCostCount2 = 1000;
 
 function countUp() {
-  maincount++;
-  number.text(maincount);
+  mainCount++;
+  number.text(mainCount);
 }
 
-AdditionButton.on("click", countUp);
+additionButton.on("click", countUp);
 
-BuyButton.on("click", () => {
-  levelcount++;
-  BuyButton.text(`level${levelcount}`);
-  intervalID = setInterval(countUp, 1000);
+buyButton.on("click", () => {
+  if (mainCount >= levelCostCount) {
+    mainCount -= levelCostCount;
+    levelCount++;
+    levelCostCount = Math.round(levelCostCount * 1.4);
+    buyButton.text(`Level${levelCount}`);
+    levelUpCost.text(`Level up cost ${levelCostCount}`);
+    intervalID = setInterval(countUp, 1000);
+  }
 });
 
-DisplayButton.on("click", () => {
+buyButton2.on("click", () => {
+  if (mainCount >= levelCostCount2) {
+    mainCount -= levelCostCount2;
+    levelCount2++;
+    levelCostCount2 = Math.round(levelCostCount2 * 1.41);
+    buyButton2.text(`Level${levelCount2}`);
+    levelUpCost2.text(`Level up cost ${levelCostCount2}`);
+    for (let i = 0; i < 10; i++) {
+      console.log("あ");
+      intervalID = setInterval(countUp, 1000);
+    }
+  }
+});
+
+displayButton.on("click", () => {
   if (shop.hasClass("shop-active")) {
     shop.removeClass("shop-active");
-    DisplayButton.removeClass("X");
-    DisplayButton.addClass("hamburger");
-    console.log(header.innerWidth());
-    while (header.width < 300) {
-      MainButton.css("margin-left", header.innerWidth());
-    }
+    displayButton.removeClass("X");
+    displayButton.addClass("hamburger");
+    header.css("width", 0);
   } else {
     shop.addClass("shop-active");
-    DisplayButton.addClass("X");
-    DisplayButton.removeClass("hamburger");
-    while (header.width > 0) {
-      MainButton.css("margin-left", header.innerwidth());
-    }
+    displayButton.addClass("X");
+    displayButton.removeClass("hamburger");
+    header.css("width", 300);
   }
 });
